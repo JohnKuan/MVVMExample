4e84b95244806bc292144284e42ec2e45d35794c
John Kuan <johnkuan@burpple.com>
2018-07-20 11:14:39 +0800

    add pod CoreStore

--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '10.0'
+platform :ios, '10.0'
 
 target 'MVVMExample' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -9,6 +9,9 @@ target 'MVVMExample' do
   
   # Binding Architecture
   pod 'Bond'
+	
+  # Core Data Wrapper
+  pod 'CoreStore'
 
   target 'MVVMExampleTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-MVVMExample/Pods-MVVMExample-frameworks.sh
+++ Pods/Target Support Files/Pods-MVVMExample/Pods-MVVMExample-frameworks.sh
@@ -144,11 +144,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bond/Bond.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CoreStore/CoreStore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Differ/Differ.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveKit/ReactiveKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bond/Bond.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/CoreStore/CoreStore.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Differ/Differ.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveKit/ReactiveKit.framework"
 fi
