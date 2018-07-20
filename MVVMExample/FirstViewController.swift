730f1e16f36bbd324f5616878f2fc56a9fb58f13
John Kuan <johnkuan@burpple.com>
2018-07-20 10:33:53 +0800

    Initial Commit

---
+++ MVVMExample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MVVMExample
+//
+//  Created by John Kuan on 20/7/18.
+//  Copyright © 2018 Burpple. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
